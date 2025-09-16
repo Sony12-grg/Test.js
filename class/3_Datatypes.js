6c5414e388a8a9836ea288e31aef6f7b29dca886
Sony12-grg <soniyagurung924@gmail.com>
2025-09-16 17:43:02 +0000

    conversion

---
+++ class/3_Datatypes.js
@@ -0,0 +1,23 @@
+let age = undefined;
+//console.log(typeof age);
+//console.log(typeof (age) );
+
+let conversion = Number (age);
+//console.log(typeof conversion);
+
+//console.log(typeof conversion);
+// console.log(conversion);
+
+// "33" number= can easily change to number: 33
+// "33abc" = Not an number (NaN)
+
+let isLoggedIn = 1;
+let loggedIn = Boolean (isLoggedIn);
+console.log (loggedIn);
+
+//1 => true, 0 =>false , ""=>false, name=>True
+
+let number= 44;
+let addnumber = String (number);
+console.log(addnumber);
+console.log (typeof addnumber);
